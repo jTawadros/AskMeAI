8f72ecc4d417187a6d0f9ea3baad97b8d510eeb6
root <root@Jon.>
2025-02-07 19:57:28 -0800

    Modify to prompt and cerr when ask is not said

--- src/main.cc
+++ src/main.cc
@@ -2,11 +2,21 @@
 #include <iostream>
 #include <string>
 
-int main (int argc, char *argv[]) {
+int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    std::cerr << "Usage: ask \"your question here\"" << std::endl;
+    return 1;
+  }
 
-  std::string sender = std::string("python3 caller.py ") + argv[1];
-  std::system(sender.c_str());
+  std::string query;
+  for (int i = 1; i < argc; ++i) {
+    query += argv[i];
+    if (i < argc - 1)
+      query += " ";
+  }
 
-  return 0;
+  std::string sender = "python3 src/caller.py \"" + query + "\"";
+  int result = std::system(sender.c_str());
+
+  return result;
 }
-
